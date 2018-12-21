870e128d2dc4fd5d202b5bd11996ff8ef19f0cd1
Arco <yiyezhihen@gmail.com>
2018-12-21 22:46:05 +0800

    Add file

---
+++ 1.3.hs
@@ -0,0 +1,32 @@
+main :: IO ()
+main = print "hello world"
+
+1
+True
+x + 4
+3 :: Float
+sort [3, 4, 6, 7]
+case Foo of True -> 1
+            False -> 2
+
+-- 註解
+
+{-
+  註解
+-}
+
+let x = 4 * a
+    y = 3 * a
+in sqrt (x ^ 2 + y ^ 2)
+
+addOne :: Int -> Int
+addOne x = x + 1
+
+welcomeMsg :: String
+welcomeMsg = "Hell World"
+
+module Main where
+
+import Data.List
+
+main = print (nub [1, 2, 3, 2, 3])
